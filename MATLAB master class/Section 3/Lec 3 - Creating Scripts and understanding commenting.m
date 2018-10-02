a1d119955978eed44c8bd0eec3005aabecfb03ea
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2018-10-02 22:33:57 +0000

    added MATLAB master class\Section 3\Lec 3 - Creating Scripts and understanding commenting.m

---
+++ MATLAB master class/Section 3/Lec 3 - Creating Scripts and understanding commenting.m
@@ -0,0 +1,16 @@
+b = 89
+C = 45 
+
+D = b + C
+
+
+% this is the declaration of the variables 
+b = 89;
+
+%this is the declaration for variable c
+C = 45; 
+
+% This is the final result
+
+D = b + C
+
