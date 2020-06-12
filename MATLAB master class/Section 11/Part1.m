402c3ed35f062dbd42a4d97e312d0c78bc2efad8
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2020-06-12 22:33:32 +0000

    added MATLAB master class\Section 11\Part1.m

---
+++ MATLAB master class/Section 11/Part1.m
@@ -0,0 +1,24 @@
+%% Main program
+% This piece of program contains the main program
+X=rand(10,1);
+%%
+% $x^2+e^{\pi i}$
+
+
+%% Sub Part
+% This is the sub part of the _fdgdsfg_ program for displaying contents
+plot(X);
+%%
+% 
+% $$e^{\pi i} + 1 = 0$$
+% 
+% $$e^{\pi i} + 1 = 0$$
+% 
+% $$e^{\pi i} + 1 = 0$$
+%% Test
+% 
+% <<FILENAME.PNG>>
+% 
+% 
+% 
+% 
