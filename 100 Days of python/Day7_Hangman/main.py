00c65e6e3e2676f3cc43a36de9ca218475de0d31
stmacebal <mateo.cebquerol@gmail.com>
2023-08-09 18:29:23 +0200

    modified script and added step 2

--- 100 Days of python/Day7_Hangman/main.py
+++ 100 Days of python/Day7_Hangman/main.py
@@ -1,16 +1,24 @@
-#Step 1 
+#Step 1 -- get random word from sample word list and get a user input (letter) then check if it is in the word
 import random as rand;
-
 word_list = ["aardvark", "baboon", "camel"]
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-randIndex = rand.randint(0,len(word_list)-1)
-chosen_word = word_list[randIndex]
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+chosen_word = rand.choice(word_list)
 guess = input("Guess a letter: ").lower()
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for x in chosen_word:
-    if x == guess:
+for letter in chosen_word:
+    if letter == guess:
         print("yes") #letter guessed is in the word
     else:
         print("no") #letter guessed is not in the word
+
+
+#Step 2 -- create blank array and if the letter guessed is part of the word then fill it in
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
+    
+for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
+
+print(display) # display array with letters
