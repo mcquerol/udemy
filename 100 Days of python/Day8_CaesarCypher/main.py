33f2f83d1db0793a53fd208ee88ad29df6d915c3
stmacebal <mateo.cebquerol@gmail.com>
2023-09-21 22:44:33 +0200

    test case

--- 100 Days of python/Day8_CaesarCypher/main.py
+++ 100 Days of python/Day8_CaesarCypher/main.py
@@ -1,11 +1,4 @@
-# inPhrase = input("Enter a sentence: ")
-# key = int(input("what is the key: "))
 
-# for x in inPhrase:
-#     asciiNum = ord(x)
-#     asciiNum+= key
-#     asciiLetter = chr(asciiNum)
-#     print(asciiLetter, end="")
 lowercase_letters = [chr(ord('a') + i) for i in range(26)]
 
 def logo():
@@ -14,18 +7,18 @@ def logo():
 def cypher(inSentence, shiftKey, cypherType):
     i = 0
     for character in inSentence:
-        # if cypherType == "encode":
-        #     asciiLetter = lowercase_letters[i+shiftKey]
-        # elif cypherType == "decode":
-        #     asciiLetter = lowercase_letters[i-shiftKey]
         if character.isalpha():
-            print(character, end="")
+            if cypherType == "encode":
+                asciiLetter = lowercase_letters[i+shiftKey]
+            elif cypherType == "decode":
+                asciiLetter = lowercase_letters[i-shiftKey]
+            print(asciiLetter, end="")
+            i = i + 1
         else:
             print(" ", end="")
-    #print(asciiLetter, end="")
 
 if __name__ == "__main__":
     #inSentence = input("Enter a sentence: ")
     #shiftKey = int(input("Type the shift number: "))
     #cypherType = input("Would you like to encode or decode?: ")
-    cypher(inSentence = "hello world", shiftKey = 3, cypherType = "decode")
+    cypher(inSentence = "hello world", shiftKey = 3, cypherType = "encode")
